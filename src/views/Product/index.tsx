6709baeb1b225769019500b720caae5146403e20
trisskmasarahh <trisukmasarahh@gmail.com>
2026-04-06 22:08:59 +0700

    jobsheet 8

--- src/views/Product/index.tsx
+++ src/views/Product/index.tsx
@@ -1,13 +1,49 @@
-import HeroSection from './HeroSection';
-import MainSection from './MainSection';
+import styles from "../../pages/produk/product.module.scss";
 
-const ProductView = () => {
-    return (
-        <div className="min-h-screen bg-gray-50">
-            <HeroSection />
-            <MainSection />
-        </div>
-    );
+
+type ProductType = {
+    id: string;
+    name: string;
+    price: number;
+    image: string;
+    category: string;
 };
 
-export default ProductView;
+const TampilanProduk = ({ products }: { products: ProductType[] }) => {
+    return (
+        <div className={styles.produk}>
+        <h1 className={styles.produk__title}>Daftar Produk</h1>
+        <div className={styles.produk__content}>
+            {products.length > 0 ? (
+            <>
+                {products.map((products: ProductType) => (
+                <div key={products.id} className={styles.produk__content__item}>
+                    <div className={styles.produk__content__item__image}>
+                    <img src={products.image} alt={products.name} width={200} />
+                    </div>
+                    <h4 className={styles.produk__content__item__name}>
+                    {products.name}
+                    </h4>
+                    <p className={styles.produk__content__item__category}>
+                    {products.category}
+                    </p>
+                    <p className={styles.produk__content__item__price}>
+                    Rp {products.price.toLocaleString()}
+                    </p>
+                </div>
+                ))}
+            </>
+            ) : (
+            <div className={styles.produk__content__skeleton}>
+                <div className={styles.produk__content__skeleton__image}></div>
+                <div className={styles.produk__content__skeleton__name}></div>
+                <div className={styles.produk__content__skeleton__category}></div>
+                <div className={styles.produk__content__skeleton__price}></div>
+            </div>
+            )}
+        </div>
+        </div>
+    );
+    };
+
+export default TampilanProduk;
